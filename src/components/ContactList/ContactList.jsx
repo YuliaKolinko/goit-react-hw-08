0d45125a0c1f962808ecb1451ae861eb45a6a2e6
Yulia <derevyanko.julia@gmail.com>
2025-06-12 21:32:50 +0300

    Fix typo in import statement and useSelector for filtered contacts in ContactList component

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,10 +1,10 @@
 import css from "./ContactList.module.css";
 import { useSelector } from "react-redux";
 import Contact from "../Contact/Contact";
-import { selectFiltredContacts } from "../../redux/contactsSlice";
+import { selectFilteredContacts } from "../../redux/contactsSlice";
 
 export default function ContactList() {
-  const filteredContacts = useSelector(selectFiltredContacts);
+  const filteredContacts = useSelector(selectFilteredContacts);
   return (
     <ul className={css.list}>
       {filteredContacts.map((contact) => (
