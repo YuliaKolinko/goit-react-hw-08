e1f79dd639badf0925e502fdda82e5f3ed09c8b0
Yulia <derevyanko.julia@gmail.com>
2025-06-13 21:18:01 +0300

    Add ContactForm component to ContactPage for creating new contacts

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchContacts } from "../../redux/contacts/operations";
 import ContactList from "../../components/ContactList/ContactList";
+import ContactForm from "../../components/ContactForm/ContactForm";
 import SearchBox from "../../components/SearchBox/SearchBox";
 import css from "./ContactPage.module.css";
 
@@ -18,6 +19,7 @@ export default function ContactPage() {
       <h3>Create new contact</h3>
       <main className={css.main}>
         <h1>Phonebook</h1>
+        <ContactForm />
         <SearchBox />
         {loading && <p>Loading contacts...</p>}
         {error && <p>Error: {error}</p>}
